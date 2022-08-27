a6bea0d08ad76e440dfa5cdebd498b156a769394
RingiBoy <mitya.info@gmail.com>
2022-08-27 22:40:21 +0300

    start hw

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
 const express = require("express");
 
-const userController = require("./controllers/user.controller");
-const userRouter= require("./routes/user.router")
+const userRouter = require("./routes/user.router");
 
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
-app.use("/users", userRouter);
 
 
 app.listen(5000, () => {
