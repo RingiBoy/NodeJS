16299fc286369e2711f6bd3bc58818001e8dfd32
RingiBoy <mitya.info@gmail.com>
2022-08-31 20:25:27 +0300

    start

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
 const express = require("express");
+const users = require("./dataBAse/users.json");
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res)=>{
-  
-})
+app.get("/users", (req, res) => {
+  res.json(users);
+});
+app.get("/users/:id", (req, res) => {
+  const userIndex = +req.params.id;
 
+  console.log("userIndex:", userIndex);
+  if (userIndex < 0 || isNaN(userIndex)) {
+    res.status(400).json("please input valid id");
+    return;
+  }
+  const user = users[userIndex];
 
+  if (!user) {
+    res.status(404).json("please input valid id");
+    return;
+  }
+  res.json(users[userIndex]);
+});
 
 
 
