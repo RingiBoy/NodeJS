1c4c025a0486fc13d720c12bf1d828b9f9c78754
RingiBoy <mitya.info@gmail.com>
2022-10-24 13:04:38 +0300

    трейн1

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const userRouter = require("./routes/user.router");
 const config = require("./config/config")
 const express = require("express");
+const { response } = require('express');
 const app = express();
 
 // console.log("my folder of project is:", cwd());
@@ -10,6 +11,15 @@ const app = express();
 app.use(express.json());
 
 app.use("/users", userRouter);
+
+app.use('*',( req, res, next)=>{
+  next(new Error('Route not found'));
+
+});
+
+app.use((err, req, res , next)=>{
+ res.json(err.message)
+})
 
 app.listen(config.PORT, () => {
   console.log("app listen 5000");
--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,57 +1,73 @@
-const fileService = require("../services/file.service")
+const fileService = require("../services/file.service");
 
 module.exports = {
   getAllUsers: async (req, res) => {
-    const users = await fileService.readDB();
-    res.json(users);
+    try {
+      const users = await fileService.readDB();
+      res.json(users);
+    } catch (error) {
+      res.json(error);
+    }
   },
   createUser: async (req, res) => {
-    const { name, age } = req.body;
+    try {
+      const { name, age } = req.body;
 
-    
+      const users = await fileService.readDB();
 
-    const users = await fileService.readDB();
+      const newUser = {
+        ...req.body,
+        id: new Date().valueOf(),
+        // id: users.length ? users[users.length - 1].id + 1 : 1
+      };
 
-    const newUser = {
-      ...req.body,
-      id: new Date().valueOf(),
-      // id: users.length ? users[users.length - 1].id + 1 : 1
-    };
+      await fileService.pushToDB([...users, newUser]);
 
-    await fileService.pushToDB([...users, newUser]);
-
-    res.status(201).json(newUser);
+      res.status(201).json(newUser);
+    } catch (error) {
+      res.json(error);
+    }
   },
   getUserById: async (req, res) => {
-    const { userId } = req.params;
-    const users = await fileService.readDB();
-    const user = await users.find((user) => user.id === +userId);
-    res.status(201).json(user);
+    try {
+      const { userId } = req.params;
+      const users = await fileService.readDB();
+      const user = await users.find((user) => user.id === +userId);
+      res.status(201).json(user);
+    } catch (error) {
+      res.json(error);
+    }
   },
   updateUser: async (req, res) => {
     // console.log('put', req.body);
+    try {
+      const { userId } = req.params;
+      const users = await fileService.readDB();
+      const updateUsers = await users.map((user) =>
+        user.id === +userId
+          ? { name: req.body.name, age: req.body.age, id: user.id }
+          : user
+      );
 
-    const { userId } = req.params;
-    const users = await fileService.readDB();
-    const updateUsers = await users.map((user) =>
-      user.id === +userId
-        ? { name: req.body.name, age: req.body.age, id: user.id }
-        : user
-    );
-
-    await fileService.pushToDB(updateUsers);
-    res.status(201).json(updateUsers);
+      await fileService.pushToDB(updateUsers);
+      res.status(201).json(updateUsers);
+    } catch (error) {
+      res.json(error);
+    }
   },
   deleteUser: async (req, res) => {
     // console.log('put', req.body);
+    try {
+      const { userId } = req.params;
+      const users = await fileService.readDB();
+      const updateUsers = await users.filter((user) => {
+        if (user.id !== +userId) return user;
+      });
 
-    const { userId } = req.params;
-    const users = await fileService.readDB();
-    const updateUsers = await users.filter((user) => {
-      if (user.id !== +userId) return user;
-    });
-
-    await fileService.pushToDB(updateUsers);
-    res.status(201).json(updateUsers);
+      await fileService.pushToDB(updateUsers);
+      res.status(201).json(updateUsers);
+    } catch (error) {
+      res.json(error);
+    }
   },
 };
